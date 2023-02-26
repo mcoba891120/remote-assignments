c145878055ed85bb45d760e1f208ff6b6691873a
王禹翔 <wangyuxiang@wangyuxiangdeMacBook-Air.local>
2023-02-26 20:02:14 +0800

    remote-week2:improve1

--- userapi/userapi.js
+++ userapi/userapi.js
@@ -29,14 +29,15 @@ connection.connect((error) => {
     console.log('Connected to database with connection ID '+ connection.user);
 
 });
-connection.query('SELECT * FROM users',(error, results, fields) => {
-    if (error) {
-        console.error('Error executing query: ' + error.stack);
-        return;
-    }
-
-    console.log('Query results: ', results)
-});
+function test() {
+    connection.query('SELECT * FROM users',(error, results, fields) => {
+        if (error) {
+            console.error('Error executing query: ' + error.stack);
+            return;
+        }
+        console.log('Query results: ', results)
+    })
+};
 
 
 app.get('/', (req, res) => {
@@ -47,8 +48,8 @@ app.listen(3001, () => {
     console.log('Server is running on port 3001');
 
 });
-app.get('/users/:id', (req, res) => {
-    const userId = req.params.id
+app.get('/users', (req, res) => {
+    const userId = req.query.id;
     connection.query('SELECT * FROM users WHERE id = ?', [userId], (error, [rows]) => {
         if (error) {
             console.log(error);
@@ -78,8 +79,7 @@ app.get('/users/:id', (req, res) => {
 app.use(bodyParser.json());
 app.post('/users',(req, res) => {
     const {name, email, password} = req.body;
-    const datedate  = new Date();
-    const date = datedate.toUTCString()
+    const date = new Date().toUTCString();
     const sql_if_email = 'SELECT COUNT(*) as count FROM users WHERE email = ?';
     connection.query(sql_if_email,[email],(error,results) => {
         if (error) {
